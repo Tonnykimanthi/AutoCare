6357793d7b7ecadd15de39cfc23ff7baf4fa43d6
Tony Km <kimaxhanthony9@gmail.com>
2024-09-02 13:18:35 +0300

    Edit Technicians section title

--- src/components/layout/Technicians.tsx
+++ src/components/layout/Technicians.tsx
@@ -7,7 +7,7 @@ import Technician from "../ui/Technician";
 const Technicians = () => {
   return (
     <section className="mt-5 p-5 max-sm:px-2">
-      <SectionTitle text="Our Expert Technicians" />
+      <SectionTitle text="Our Technicians" />
       <div className="mt-4 grid grid-cols-4 gap-5 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 place-items-center">
         {techniciansList.map((technician, index) => (
           <Technician technician={technician} key={index} />
